07986fb52a9551357ebd70d7d1cb16cf26284171
Karishma <karishma.7022@gmail.com>
2026-05-12 17:05:06 +0530

    feat: add recipe data

--- react_RecipeApp/src/App.jsx
+++ react_RecipeApp/src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from "./components/Navbar";
 import RecipeCard from "./components/RecipeCard";
 import RecipeDetail from "./components/RecipeDetail";
 import RecipeList from "./components/RecipeList";
+import recipes from "./data";
 
 function App(){
+  console.log(recipes);
   return(
     <div>
       <Navbar/>
---
+++ react_RecipeApp/src/data.js
@@ -0,0 +1,28 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Pancakes",
+    category: "breakfast",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Pound_layer_cake.jpg/800px-Pound_layer_cake.jpg",
+    ingredients: ["1 cup flour", "2 eggs", "1 cup milk", "2 tbsp butter"],
+    steps: ["Mix dry ingredients", "Add wet ingredients", "Cook on pan for 2 mins each side"]
+  },
+  {
+    id: 2,
+    title: "Grilled Chicken",
+    category: "lunch",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/800px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+    ingredients: ["2 chicken breasts", "olive oil", "garlic", "salt", "pepper"],
+    steps: ["Marinate chicken", "Grill for 6 mins each side", "Rest for 5 mins"]
+  },
+  {
+    id: 3,
+    title: "Pasta",
+    category: "dinner",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg",
+    ingredients: ["200g pasta", "tomato sauce", "garlic", "basil", "parmesan"],
+    steps: ["Boil pasta", "Make sauce", "Combine and serve"]
+  }
+];
+
+export default recipes;
