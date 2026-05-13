71274e5d62e71adb0a94efdb2087d1e17b1e0a64
Karishma <karishma.7022@gmail.com>
2026-05-13 17:00:45 +0530

    feat: add search input with useRef auto-focus

--- react_RecipeApp/src/components/Navbar.jsx
+++ react_RecipeApp/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect ,useRef } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function Navbar(){
     const {theme, toggleTheme} = useContext(ThemeContext);
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current.focus();
+    },[]);
 
     return(
         <div>
             <h1> Recipe App </h1>
+            <input ref={inputRef} placeholder="Search recipes..."/>
             <button onClick={toggleTheme}>
                 {theme === "light" ? "🌙 Dark" : "☀️ Light"}
             </button>
