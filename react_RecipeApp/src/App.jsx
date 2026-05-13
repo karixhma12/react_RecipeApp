0447f4a9e9f318a9b21ac6e3e68f503c77716fd7
Karishma <karishma.7022@gmail.com>
2026-05-13 11:57:46 +0530

    feat: filter recipes by category

--- react_RecipeApp/src/App.jsx
+++ react_RecipeApp/src/App.jsx
@@ -8,11 +8,20 @@ import { useState } from "react";
 function App(){
 
 const [selectedRecipe,setSelectedRecipe] = useState(null);
+const [filter,setFilter] = useState("all");
+
+  const filteredRecipes = filter=="all" ? recipes : recipes.filter((recipe)=>{
+    return recipe.category === filter;
+  })
 
   return(
     <div>
       <Navbar/>
-      <RecipeList setSelectedRecipe={setSelectedRecipe} recipes={recipes}/>
+      <button onClick={()=>setFilter("all")}> All </button>
+      <button onClick={()=>setFilter("breakfast")}> Breakfast </button>
+      <button onClick={()=>setFilter("lunch")}> Lunch </button>
+      <button onClick={()=>setFilter("dinner")}> Dinner </button>
+      <RecipeList setSelectedRecipe={setSelectedRecipe} recipes={filteredRecipes}/>
       <RecipeDetail selectedRecipe={selectedRecipe}/>
     </div>
   )
